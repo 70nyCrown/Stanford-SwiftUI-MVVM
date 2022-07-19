563e84b55b73b6e694a3d8f42a74bdcc8909d168
70nyCrown <70nycrown@gmail.com>
2022-07-19 14:09:58 +0300

    Initial Commit

---
+++ Stanford SwiftUI MVVM/Stanford_SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stanford_SwiftUI_MVVMApp.swift
+//  Stanford SwiftUI MVVM
+//
+//  Created by Антон Воронин on 19.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Stanford_SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
